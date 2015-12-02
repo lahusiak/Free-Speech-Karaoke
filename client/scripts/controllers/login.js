fad6efb94e26cf3165f6bcdec9896406f9c3ae80
lahusiak <lahusiak@gmail.com>
2015-12-02 16:03:21 -0600

    Cool facebook login/logout, shifting to server side now

--- client/scripts/controllers/login.js
+++ client/scripts/controllers/login.js
@@ -1,11 +1,12 @@
 myApp.controller('login', ['$scope', function($scope){
- $scope.name ="Login please";
+ $scope.name ="Login with Facebook";
     $scope.FBLogin = function(){
         FB.login(function(response) {
             if (response.authResponse) {
                 console.log('Welcome!  Fetching your information.... ');
                 FB.api('/me', function(response) {
                     console.log('Good to see you, ' + response.name + '.');
+                    console.log(response);
 
                     var accessToken = FB.getAuthResponse();
                     console.log(accessToken);
@@ -13,7 +14,14 @@ myApp.controller('login', ['$scope', function($scope){
             } else {
                 console.log('User cancelled login or did not fully authorize.');
             }
+
         });
-    }
+    };
+
+    //$scope.FBlogout = function(){
+    //    FB.logout(function(response) {
+    //        // user is now logged out
+    //    });
+    //}
 }]);
 
